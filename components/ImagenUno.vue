fab05c4948496cb58ee02e2a693974d50f0e2622
Nabil014 <nallis014@gmail.com>
2023-12-07 14:01:57 -0300

    feat: add solution pruebados

---
+++ components/ImagenUno.vue
@@ -0,0 +1,27 @@
+<template>
+    <v-container class="container">
+        <v-layout row>
+            <v-flex>
+                <v-img class="logo" src="https://png.pngtree.com/png-clipart/20201208/ourmid/pngtree-red-leaf-decoration-material-png-image_2520393.jpg"> </v-img>
+            </v-flex>
+            <v-divider vertical></v-divider>
+            <v-flex>
+                <div>125.33€</div>
+            </v-flex>
+        </v-layout>
+    </v-container>
+</template>
+
+<script></script>
+
+<style>
+.container {
+    justify-content: center;
+    display: flex;
+    align-items: center;
+}
+.logo {
+    height: 50px;
+    width: 50px;
+}
+</style>
--- pages/pruebados.vue
+++ pages/pruebados.vue
@@ -5,6 +5,7 @@
                 Crear un componente e importarlo en ésta page con un elemento lo mas parecido posible a la siguiente imagen:
                 <a class="ml-3" href="https://drive.google.com/file/d/1slsgbU68eEpgXo9aNmBTwR3RyK7mKHHr/view?usp=drive_link" target="_blank"> Imagen 1</a>
             </v-list-item>
+            <ImagenUno></ImagenUno>
             <v-list-item>
                 Crear un componente e importarlo en ésta page con un elemento lo mas parecido posible a la siguiente imagen:
                 <a class="ml-3" href="https://drive.google.com/file/d/1XLL9HO2LB-IaMrDh0b2DnQSlwBzzp59C/view?usp=drive_link" target="_blank"> Imagen 2</a>
