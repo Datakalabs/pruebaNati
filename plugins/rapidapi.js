a689111587b4d1b136d4f7ae190b07474c2eadc1
Nabil014 <nallis014@gmail.com>
2023-12-07 14:01:21 -0300

    feat: add solution pruebauno

--- plugins/rapidapi.js
+++ plugins/rapidapi.js
@@ -2,6 +2,8 @@ export default ({ app }, inject) => {
     const vars = {
         key: '374f3c56c1mshd20fa3302637f9dp187329jsn63a5b9dab1b0',
         avatars_host: 'doppelme-avatars.p.rapidapi.com',
+        songs_host: 'spotify-web2.p.rapidapi.com',
+        track_id_host: 'spotify-web2.p.rapidapi.com',
     }
     inject('rapidapi', vars)
 }
--- store/ejemplo.js
+++ store/ejemplo.js
@@ -10,6 +10,14 @@ export const state = () => ({
     assets: null,
     generated_avatar: null,
     error_avatar: false,
+
+    searched_song: null, // creo los diferentes estados que luego voy a consumir en el componente
+    loading_song: false,
+    error_song: false,
+    track_credits: null,
+    track_id: null,
+    loading_Track_Credits: null,
+    error_Track_Credits: null,
 })
 
 export const actions = {
@@ -91,9 +99,62 @@ export const actions = {
                 commit('updateLoadingAvatar', false)
             })
     },
+    A_SEARCH_SONG({ commit }, data) {
+        // hago el fetching a la api para traer el artista
+        commit('updateErrorSong', false) // inicializo el estado de error en false
+        commit('updateLoadingSong', true) // inicializo el estado de loading en true mientras se realiza el fetching a la API
+        this.$axios
+            .get('https://spotify-web2.p.rapidapi.com/search/?q=' + data, {
+                headers: {
+                    'X-RapidAPI-Key': this.$rapidapi.key,
+                    'X-RapidAPI-Host': this.$rapidapi.songs_host,
+                },
+            })
+            .then((res) => {
+                commit('updateLoadingSong', false)
+                if (res.data.error) {
+                    commit('updateErrorSong', true)
+                } else {
+                    commit('updateErrorSong', false)
+                    commit('updateTrackId', res.data.tracks.id)
+                    if (res.data.tracks) {
+                        commit('updateSearchedSong', res.data.tracks)
+                    } else {
+                        commit('updateSearchedSong', null)
+                    }
+                }
+            })
+            .catch(() => {
+                commit('updateLoadingSong', false)
+            })
+    },
+    A_GET_TRACK_CREDITS({ commit }, id) {
+        // hago el fetching a la api para traer los créditos de una canción en particular
+        commit('updateLoadingTrackCredits', true)
+        this.$axios
+            .get('https://spotify-web2.p.rapidapi.com/track_credits/?id=' + id, {
+                headers: {
+                    'X-RapidAPI-Key': this.$rapidapi.key,
+                    'X-RapidAPI-Host': this.$rapidapi.track_id_host,
+                },
+            })
+            .then((res) => {
+                commit('updateLoadingTrackCredits', false)
+                if (res.data.error) {
+                    commit('updateErrorTrackCredits', true)
+                } else {
+                    commit('updateErrorTrackCredits', false)
+                    commit('updateTrackCredits', res.data)
+                }
+            })
+            .catch(() => {
+                commit('updateLoadingTrackCredits', false)
+            })
+    },
 }
 
 export const mutations = {
+    // cargo los diferentes estados
     updateLoadingBodies(state, data) {
         state.loading_bodies = data
     },
@@ -120,5 +181,27 @@ export const mutations = {
     },
     updateErrorAvatar(state, data) {
         state.error_avatar = data
+    },
+
+    updateErrorSong(state, data) {
+        state.error_song = data
+    },
+    updateLoadingSong(state, data) {
+        state.loading_song = data
+    },
+    updateSearchedSong(state, data) {
+        state.searched_song = data
+    },
+    updateTrackId(state, data) {
+        state.track_id = data
+    },
+    updateTrackCredits(state, data) {
+        state.track_credits = data
+    },
+    updateLoadingTrackCredits(state, data) {
+        state.loading_Track_Credits = data
+    },
+    updateErrorTrackCredits(state, data) {
+        state.error_Track_Credits = data
     },
 }
